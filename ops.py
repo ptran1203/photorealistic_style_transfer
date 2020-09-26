eb5761d3bc774cf6100ee3d8ee35cc166d25c259
phattran <phatth1203@gmail.com>
2020-09-27 00:26:50 +0700

    wct

--- ops.py
+++ ops.py
@@ -1,6 +1,6 @@
 
 from keras.layers import Layer
-from tensorflow.nn import conv2d, conv2d_transpose
+import tensorflow as tf
 import numpy as np
 
 class WaveLetPooling(Layer):
@@ -20,16 +20,16 @@ class WaveLetPooling(Layer):
         self.repeat_filters(inputs.shape[-1])
         print(self.LL.shape)
         if self.upsample:
-            return (conv2d_transpose(inputs, self.LL, strides=[1, 2, 2, 1], padding='SAME'),
-                    conv2d_transpose(inputs, self.LH, strides=[1, 2, 2, 1], padding='SAME'),
-                    conv2d_transpose(inputs, self.HL, strides=[1, 2, 2, 1], padding='SAME'),
-                    conv2d_transpose(inputs, self.HH, strides=[1, 2, 2, 1], padding='SAME'))
+            return (tf.nn.conv2d_transpose(inputs, self.LL, strides=[1, 2, 2, 1], padding='SAME'),
+                    tf.nn.conv2d_transpose(inputs, self.LH, strides=[1, 2, 2, 1], padding='SAME'),
+                    tf.nn.conv2d_transpose(inputs, self.HL, strides=[1, 2, 2, 1], padding='SAME'),
+                    tf.nn.conv2d_transpose(inputs, self.HH, strides=[1, 2, 2, 1], padding='SAME'))
 
 
-        return (conv2d(inputs, self.LL, strides=[1, 2, 2, 1], padding='SAME'),
-                conv2d(inputs, self.LH, strides=[1, 2, 2, 1], padding='SAME'),
-                conv2d(inputs, self.HL, strides=[1, 2, 2, 1], padding='SAME'),
-                conv2d(inputs, self.HH, strides=[1, 2, 2, 1], padding='SAME'))
+        return (tf.nn.conv2d(inputs, self.LL, strides=[1, 2, 2, 1], padding='SAME'),
+                tf.nn.conv2d(inputs, self.LH, strides=[1, 2, 2, 1], padding='SAME'),
+                tf.nn.conv2d(inputs, self.HL, strides=[1, 2, 2, 1], padding='SAME'),
+                tf.nn.conv2d(inputs, self.HH, strides=[1, 2, 2, 1], padding='SAME'))
 
 
     def repeat_filters(self, repeats):
@@ -37,3 +37,66 @@ class WaveLetPooling(Layer):
         self.LH = np.transpose(np.repeat(self.LH, repeats, axis=0), (1, 2, 3, 0))
         self.HL = np.transpose(np.repeat(self.HL, repeats, axis=0), (1, 2, 3, 0))
         self.HH = np.transpose(np.repeat(self.HH, repeats, axis=0), (1, 2, 3, 0))
+
+
+def WhiteningAndColoring(Layer):
+    def __init__(self):
+        super(WhiteningAndColoring, self).__init__()
+
+
+    def call(self, inputs):
+        """
+        Make it works first .
+        """
+        content, style = inputs
+        
+        content_t = tf.transpose(tf.squeeze(content), (2, 0, 1))
+        style_t = tf.transpose(tf.squeeze(style), (2, 0, 1))
+
+        Cc, Hc, Wc = tf.unstack(tf.shape(content_t))
+        Cs, Hs, Ws = tf.unstack(tf.shape(style_t))
+
+        # CxHxW -> CxH*W
+        content_flat = tf.reshape(content_t, (Cc, Hc*Wc))
+        style_flat = tf.reshape(style_t, (Cs, Hs*Ws))
+
+        # Content covariance
+        mc = tf.reduce_mean(content_flat, axis=1, keep_dims=True)
+        fc = content_flat - mc
+        fcfc = tf.matmul(fc, fc, transpose_b=True) / (tf.cast(Hc*Wc, tf.float32) - 1.) + tf.eye(Cc)*eps
+
+        # Style covariance
+        ms = tf.reduce_mean(style_flat, axis=1, keep_dims=True)
+        fs = style_flat - ms
+        fsfs = tf.matmul(fs, fs, transpose_b=True) / (tf.cast(Hs*Ws, tf.float32) - 1.) + tf.eye(Cs)*eps
+
+        # tf.svd is slower on GPU, see https://github.com/tensorflow/tensorflow/issues/13603
+        with tf.device('/cpu:0'):  
+            Sc, Uc, _ = tf.svd(fcfc)
+            Ss, Us, _ = tf.svd(fsfs)
+
+        # Filter small singular values
+        k_c = tf.reduce_sum(tf.cast(tf.greater(Sc, 1e-5), tf.int32))
+        k_s = tf.reduce_sum(tf.cast(tf.greater(Ss, 1e-5), tf.int32))
+
+        # Whiten content feature
+        Dc = tf.diag(tf.pow(Sc[:k_c], -0.5))
+        fc_hat = tf.matmul(tf.matmul(tf.matmul(Uc[:,:k_c], Dc), Uc[:,:k_c], transpose_b=True), fc)
+
+        # Color content with style
+        Ds = tf.diag(tf.pow(Ss[:k_s], 0.5))
+        fcs_hat = tf.matmul(tf.matmul(tf.matmul(Us[:,:k_s], Ds), Us[:,:k_s], transpose_b=True), fc_hat)
+
+        # Re-center with mean of style
+        fcs_hat = fcs_hat + ms
+
+        # Blend whiten-colored feature with original content feature
+        blended = alpha * fcs_hat + (1 - alpha) * (fc + mc)
+
+        # CxH*W -> CxHxW
+        blended = tf.reshape(blended, (Cc,Hc,Wc))
+        # CxHxW -> 1xHxWxC
+        blended = tf.expand_dims(tf.transpose(blended, (1,2,0)), 0)
+
+        return blended
+
