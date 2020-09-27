22b20d2ecd006b3254ddb113db3150afe67a9879
phattran <phatth1203@gmail.com>
2020-09-28 00:12:43 +0700

    print

--- ops.py
+++ ops.py
@@ -18,15 +18,21 @@ class WaveLetPooling(Layer):
             tf.math.multiply(square_of_2, tf.constant([[-1, 1]], dtype=tf.float32))
         )
 
+        print(L)
+        print(H)
+
         self.LL = tf.reshape(tf.math.multiply(tf.transpose(L), L), (1, 2, 2, 1))
         self.LH = tf.reshape(tf.math.multiply(tf.transpose(L), H), (1, 2, 2, 1))
         self.HL = tf.reshape(tf.math.multiply(tf.transpose(H), L), (1, 2, 2, 1))
         self.HH = tf.reshape(tf.math.multiply(tf.transpose(H), H), (1, 2, 2, 1))
 
+        print(self.LL)
+
 
     def call(self, inputs):
         self.repeat_filters(inputs.shape[-1])
         input_shape = K.int_shape(inputs)
+        print(self.LL)
         if self.upsample:
             output_shape = (1, input_shape[1] * 2,
                             input_shape[2] * 2, input_shape[3])
