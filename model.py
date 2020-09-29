3caffc0965f12d26815a558d974365b07261ea45
phattran <phat.tranhoang@cyberlogitec.com>
2020-09-29 13:56:48 +0700

    h

--- model.py
+++ model.py
@@ -41,9 +41,9 @@ class WCT2:
         self.wct = self.build_wct_model()
         # ======= Loss functions ======= #
         recontruct_img = self.wct(img)
-        feat = self.encoder(img)
+        # feat = self.encoder(img)
 
-        self.trainer = Model(inputs=[img], outputs=[recontruct_img, feat], name="trainer")
+        self.trainer = Model(inputs=[img], outputs=[recontruct_img], name="trainer")
         self.trainer.compile(optimizer=Adam(self.lr), loss=["mse", "mse"])
 
 
@@ -130,7 +130,7 @@ class WCT2:
             batch_loss = self.init_hist()
             for content_img in data_gen.next_batch():
                 feat = self.encoder.predict(content_img)
-                loss, *_ = self.trainer.train_on_batch([content_img], [content_img, feat])
+                loss, *_ = self.trainer.train_on_batch([content_img], [content_img])
                 batch_loss['loss'].append(loss)
 
             # evaluate
