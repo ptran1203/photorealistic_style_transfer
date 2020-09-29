72f98e90d94d90776eb2ad482dfa6e9d096edb2a
phattran <phat.tranhoang@cyberlogitec.com>
2020-09-29 13:21:02 +0700

    la

--- model.py
+++ model.py
@@ -38,13 +38,12 @@ class WCT2:
         self.show_interval = show_interval
         self.img_shape = (self.rst, self.rst, 3)
         img = Input(self.img_shape)
-        self.build_wct_model()
         self.wct = self.build_wct_model()
         # ======= Loss functions ======= #
         recontruct_img = self.wct([img])
         gen_feat = self.encoder(recontruct_img)
 
-        self.trainer = Model(inputs=img, outputs=[recontruct_img, gen_feat], name="trainer")
+        self.trainer = Model(inputs=[img], outputs=[recontruct_img, gen_feat], name="trainer")
         self.trainer.compile(optimizer=Adam(self.lr), loss=["mse", "mse"])
 
 
@@ -130,7 +129,7 @@ class WCT2:
             batch_loss = self.init_hist()
             for content_img in data_gen.next_batch():
                 feat = self.encoder.predict(content_img)
-                loss, *_ = self.trainer.train_on_batch(content_img, [content_img, feat])
+                loss, *_ = self.trainer.train_on_batch([content_img], [content_img, feat])
                 batch_loss['loss'].append(loss)
 
             # evaluate
