06f3b1e36373e416ff8f5e936e185b1a0876b71e
phattran <phat.tranhoang@cyberlogitec.com>
2020-09-30 09:57:19 +0700

    rmvar

--- ops.py
+++ ops.py
@@ -168,7 +168,7 @@ class Reduction(tf.keras.layers.Layer):
         return tf.reduce_sum(inputs)
 
 
-def _conv2d_transpose(x, kernel, output_shape, name):
+def _conv2d_transpose(x, kernel, output_shape):
     conv = tf.nn.conv2d_transpose(
             x, kernel,
             output_shape=output_shape,
@@ -177,7 +177,7 @@ def _conv2d_transpose(x, kernel, output_shape, name):
     return conv
 
 
-def _conv2d(x, kernel, name):
+def _conv2d(x, kernel):
     conv = tf.nn.conv2d(x, kernel, strides=[1, 2, 2, 1], padding='SAME')
     return conv
 
